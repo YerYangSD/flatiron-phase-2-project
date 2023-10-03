e0b3b00017005ce03a564ff949938b570e38fa65
Yer Yang <yyhmg_86@yahoo.com>
2023-10-02 23:01:22 -0700

    import components to App component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom/cjs/react-router-dom.min';
+import NavBar from "./NavBar"
+import Home from "./Home"
+import User from "./User"
+import SignUp from "./SignUp"
 
 function App() {
   return (

e94d2249c877a5b40c39bb5facab4663f03005ad
Yer Yang <yyhmg_86@yahoo.com>
2023-10-04 01:13:38 -0700

    modifies App path in index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './app/App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom/cjs/react-router-dom.min';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+// Use <BrowserRouter> component to wrap the entire application and provide React Router context features
 root.render(<BrowserRouter><App /></BrowserRouter>);
 
 // If you want to start measuring performance in your app, pass a function

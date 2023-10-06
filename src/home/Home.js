777818bf80937be3fe77b1d9adcb2aa3617f0553
Yer Yang <yyhmg_86@yahoo.com>
2023-10-06 12:59:19 -0700

    adds a brief description of the app

--- src/home/Home.js
+++ src/home/Home.js
@@ -2,9 +2,10 @@ import React from "react";
 
 function Home() {
     return (
-        <>
-            <h1>Welcome</h1>
-        </>
+        <div id="home">
+            <h1>Welcome to Slotmoji!</h1>
+            <p>Slotmoji is a delightful and engaging mobile application that brings a playful twist to the world of slot machines. With Slotmoji, users can experience the thrill of traditional slot games while injecting their own fun and personality through emojis. This app allows you to customize the spinning reels with your favorite emojis, turning every spin into a unique and entertaining experience. Watch as your chosen emojis align to create winning combinations, all while enjoying the colorful and dynamic graphics. Whether you're a seasoned gambler or just looking for a lighthearted way to pass the time, Slotmoji offers a joyful and emoji-filled adventure that's sure to put a smile on your face. Try your luck and spin to win with the power of emojis at your fingertips!</p>
+        </div>
     )
 }
 
